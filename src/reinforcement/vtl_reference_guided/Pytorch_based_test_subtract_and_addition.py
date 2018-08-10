e15ca904bc95d96dcaaadf3054e003fbcb938760
Denis Shitov <S3628075@student.rmit.edu.au>
2018-08-11 00:11:27 +1000

    DS: add pytorch implementation of the same test model tynamics and policy networks.
    Works well

--- src/reinforcement/vtl_reference_guided/Pytorch_based_test_subtract_and_addition.py
+++ src/reinforcement/vtl_reference_guided/Pytorch_based_test_subtract_and_addition.py
@@ -5,6 +5,10 @@ from sklearn.metrics import mean_squared_error
 import numpy as np
 import tensorflow as tf
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 ds_norm_largest = 1000.
 ds_largest = 1000.
 largest = 1000.
@@ -69,70 +73,71 @@ n_epoch = 50
 # Model Dynamics
 ################################################################
 
-# create model dynamics
 
-# specify input placeholders
-md_input = tf.placeholder(tf.float32, [None, n_dim * 2])
+class ModelDynamicsNet(nn.Module):
+    def __init__(self, n_dim):
+        super(ModelDynamicsNet, self).__init__()
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(n_dim * 2, n_dim * 4)
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
 
-# specify network structure
-md_initializer = tf.glorot_uniform_initializer()
-md_dense1 = tf.layers.dense(inputs=md_input, units=4 * n_dim, kernel_initializer=md_initializer)
-md_dense2 = tf.layers.dense(inputs=md_dense1, units=2 * n_dim, kernel_initializer=md_initializer)
+        self.fc2 = nn.Linear(n_dim * 4, n_dim * 2)
+        torch.nn.init.xavier_uniform_(self.fc2.weight)
 
-# specify output
-md_output = tf.layers.dense(inputs=md_dense2, units=1 * n_dim, kernel_initializer=md_initializer)
+        self.fc3 = nn.Linear(n_dim * 2,  n_dim)
+        torch.nn.init.xavier_uniform_(self.fc3.weight)
 
-# network target for training
-md_target = tf.placeholder(tf.float32, [None, n_dim * 1])
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
 
-# loss
-md_loss = tf.losses.mean_squared_error(md_target, md_output)
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
 
-# train step
-md_global_step = tf.train.create_global_step()
-md_train_step = tf.train.AdadeltaOptimizer(learning_rate=1.0).minimize(loss=md_loss, global_step=md_global_step)
+md_net = ModelDynamicsNet(n_dim)
+md_criterion = nn.MSELoss()
+md_optimizer = torch.optim.Adadelta(md_net.parameters())
 
 ################################################################
 # Policy
 ################################################################
 
-# create policy
-with tf.variable_scope('policy'):
-    # specify input placeholders
-    policy_input = tf.placeholder(tf.float32, [None, n_dim * 2])
-
-    # specify network structure
-    policy_initializer = tf.glorot_uniform_initializer()
-    policy_dense1 = tf.layers.dense(inputs=policy_input, units=4 * n_dim, kernel_initializer=policy_initializer)
-    policy_dense2 = tf.layers.dense(inputs=policy_dense1, units=2 * n_dim, kernel_initializer=policy_initializer)
-
-    # specify output
-    policy_output = tf.layers.dense(inputs=policy_dense2, units=1 * n_dim, kernel_initializer=policy_initializer)
-
-    # network target for training
-    policy_target = tf.placeholder(tf.float32, [None, n_dim * 1])
-
-    # trainable variables
-    trainable_vars = tf.trainable_variables('policy')
-
-    # train step
-    policy_global_step = tf.Variable(0., trainable=False)
-
-    ############################################
-    # Policy gradient based optimization routine
-    ############################################
-
-    policy_md_loss = md_loss
-    action_grads = tf.gradients(ys=policy_md_loss, xs=md_input)[0][:, n_dim:]
-    policy_opt = tf.train.AdadeltaOptimizer(learning_rate=1.0)
-    optimize_op = policy_opt.minimize(loss=policy_output, global_step=policy_global_step, var_list=trainable_vars,
-                                      grad_loss=action_grads)
 
 
+class PolicyNet(nn.Module):
+    def __init__(self, n_dim):
+        super(PolicyNet, self).__init__()
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(n_dim * 2, n_dim * 4)
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
 
-# Open the session
-sess = tf.InteractiveSession()
-sess.run(tf.global_variables_initializer())
+        self.fc2 = nn.Linear(n_dim * 4, n_dim * 2)
+        torch.nn.init.xavier_uniform_(self.fc2.weight)
+
+        self.fc3 = nn.Linear(n_dim * 2,  n_dim)
+        torch.nn.init.xavier_uniform_(self.fc3.weight)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+policy_net = PolicyNet(n_dim)
+policy_optimizer = torch.optim.Adadelta(policy_net.parameters())
 
 
 ########################################################
@@ -149,21 +154,27 @@ for _ in range(n_epoch):
         x_batch = X[i * n_batch : (i + 1) * n_batch]
         y_batch = Y[i * n_batch: (i + 1) * n_batch]
 
+        md_optimizer.zero_grad()  # Intialize the hidden weight to all zeros
+        outputs = md_net(torch.from_numpy(x_batch).float())
+        loss = md_criterion(outputs,
+                            torch.from_numpy(y_batch).float())
+        loss.backward()  # Backward pass: compute the weight
         # train step
-        feed_dict = {md_input: x_batch, md_target: y_batch}
-        md_train_step.run(feed_dict=feed_dict)
+        md_optimizer.step()
+
 
     # calculate loss on the whole dataset
-    y_pred = sess.run(md_output, feed_dict={md_input:X})
+    y_pred = md_net(torch.from_numpy(X).float()).detach().numpy()
     total_loss = np.mean(np.sum(np.square(Y - y_pred), axis=1), axis=0)
     y_pred_denormed = denormalize(y_pred, largest)
     y_denormed = denormalize(Y, largest)
     total_loss_denormed = np.mean(np.sum(np.square(y_denormed - y_pred_denormed), axis=1), axis=0)
-    print('|epoch : {}| model dynamics loss: {:.10f}| unnormalized loss: {:.7f}'.format(_, total_loss, total_loss_denormed))
+    print('|epoch : {}| model dynamics loss: {:.10f}| unnormalized loss: {:.7f}'
+          .format(_, total_loss, total_loss_denormed))
 
 # Final evaluation on some new patterns
 X, Y = generate_model_dynamics_training_data(n_examples)
-y_pred = sess.run(md_output, feed_dict={md_input:X})
+y_pred = md_net(torch.from_numpy(X).float()).detach().numpy()
 # calculate error
 expected = denormalize(Y, largest)
 predicted = denormalize(y_pred, largest)
@@ -191,14 +202,21 @@ for _ in range(n_epoch):
         s0_batch = s0[i * n_batch: (i + 1) * n_batch]
         t_batch = t[i * n_batch: (i + 1) * n_batch]
 
+        policy_optimizer.zero_grad()
         # train step
-        y_pred = sess.run(policy_output, feed_dict={policy_input: x_batch})
-        md_x_batch = np.concatenate((s0_batch, y_pred), axis=1)
+        y_pred = policy_net(torch.from_numpy(x_batch).float())
+        md_x_batch = torch.cat((torch.from_numpy(s0_batch).float(), y_pred), 1)
+        md_pred = md_net(md_x_batch)
 
-        feed_dict = {policy_input: x_batch, md_input: md_x_batch, md_target: t_batch}
-        optimize_op.run(feed_dict=feed_dict)
+        loss = md_criterion(md_pred, torch.from_numpy(t_batch).float())
+        action_grads = torch.autograd.grad(loss, y_pred)
+
+        torch.autograd.backward(y_pred, action_grads)
+        policy_optimizer.step()
+
+
     # calculate loss on the whole dataset
-    y_pred = sess.run(policy_output, feed_dict={policy_input: X})
+    y_pred = policy_net(torch.from_numpy(X).float()).detach().numpy()
     total_loss = np.mean(np.sum(np.square(Y - y_pred), axis=1), axis=0)
     y_pred_denormed = denormalize(y_pred, ds_norm_largest)
     y_denormed = denormalize(Y, ds_norm_largest)
@@ -207,7 +225,7 @@ for _ in range(n_epoch):
 
 # Final evaluation on some new patterns
 X, s0, t, Y = generate_policy_training_data(n_examples)
-y_pred = sess.run(policy_output, feed_dict={policy_input:X})
+y_pred = policy_net(torch.from_numpy(X).float()).detach().numpy()
 # calculate error
 expected = denormalize(Y, ds_norm_largest)
 predicted = denormalize(y_pred, ds_norm_largest)
--- src/reinforcement/vtl_reference_guided/tf_train.py
+++ src/reinforcement/vtl_reference_guided/tf_train.py
@@ -101,10 +101,6 @@ def train(settings, env, replay_buffer, reference_trajectory):
 
     # setup all summaries and dump directories
 
-
-
-
-
     num_episodes = 50000
     action_noise = OrnsteinUhlenbeckActionNoise(mu=np.zeros(env.action_dim), sigma=0.001)
     s_dim = settings['state_dim']
@@ -186,8 +182,6 @@ def train(settings, env, replay_buffer, reference_trajectory):
     sess = tf.InteractiveSession()
     sess.run(tf.global_variables_initializer())
 
-
-
     saver = tf.train.Saver(tf.global_variables())
     dt = str(datetime.datetime.now().strftime("%m_%d_%Y_%I_%M_%p"))
     writer = tf.summary.FileWriter(settings['summary_dir'] + '/summary_md_' + dt, sess.graph)
@@ -251,7 +245,7 @@ def train(settings, env, replay_buffer, reference_trajectory):
             s0 = s1
             g0 = g1
             # some sort of early stopping
-            # Need to change for sure in order to avoid going out of bound at least
+            # Need to change for sure in order to avoid going out of bounds at least
             if max(miss) > 0.1 and i % 200 != 0:
                 break
         # dump video
@@ -272,7 +266,6 @@ def train(settings, env, replay_buffer, reference_trajectory):
             s1_batch_normed = normalize(s1_batch, s_bound)
             g1_batch_normed = normalize(g1_batch, g_bound)
             target_batch_normed = normalize(target_batch, g_bound)
-
 
             # train model dynamics
             md_input_X = np.concatenate((s0_batch_normed,
@@ -319,17 +312,6 @@ def train(settings, env, replay_buffer, reference_trajectory):
             # writer.add_summary(summary_str, i)
             # writer.flush()
 
-            # print(' Episode: {:d} |'
-            #       ' Policy loss: {:.4f}'
-            #       ' Policy cosine loss: {:.4f}'
-            #       ' Policy squared error loss: {:.4f}'
-            #       ' Model dynamics loss: {:.4f}|'
-            #       ' MD goal loss: {:.4f}'.format(i,
-            #                                      policy_se_loss,
-            #                                      policy_cos_loss,
-            #                                      policy_se_loss,
-            #                                       md_loss,
-            #                                       md_goal_loss))
 
 def main():
     speaker_fname = os.path.join(r'C:\Study\SpeechAcquisitionModel\src\VTL', 'JD2.speaker')
@@ -357,7 +339,6 @@ def main():
             'videos_dir': r'C:\Study\SpeechAcquisitionModel\reports\videos'
         }
 
-
     replay_buffer = ReplayBuffer(100000)
 
     reference_fname = r'C:\Study\SpeechAcquisitionModel\src\VTL\references\a_i.pkl'
@@ -372,212 +353,3 @@ if __name__ == '__main__':
     np.random.seed(1)
     tf.set_random_seed(1)
     main()
-
-#
-# def generate_model_dynamics_training_data(n_examples):
-#     s0 = np.array([[(rnd() - 0.5) * 1. * largest for n in range(n_dim)] for _ in range(n_examples)])
-#     ds = np.array([[(rnd() - 0.5) * 1. * ds_largest for n in range(n_dim)] for _ in range(n_examples)])
-#     t = s0 + ds
-#     a = np.array([[(rnd() - 0.5) * 1. * ds_largest for n in range(n_dim)] for _ in range(n_examples)])
-#
-#     # real model dynamics
-#     s1 = s0 + a
-#
-#     # normalise staff
-#     s0 = normalize(s0, largest)
-#     t = normalize(t, largest)
-#     a = normalize(a, ds_norm_largest)
-#     s1 = normalize(s1, largest)
-#
-#     X_md = np.concatenate((s0, a), axis=1)
-#     Y_md = s1
-#
-#     return X_md, Y_md
-#
-#
-# def generate_policy_training_data(n_examples):
-#     s0 = np.array([[(rnd() - 0.5) * 1. * largest for n in range(n_dim)] for _ in range(n_examples)])
-#     ds = np.array([[(rnd() - 0.5) * 1. * ds_largest for n in range(n_dim)] for _ in range(n_examples)])
-#     t = s0 + ds
-#     # normalise staff
-#     s0 = normalize(s0, largest)
-#     t = normalize(t, largest)
-#
-#     X = np.concatenate((s0, t), axis=1)
-#
-#     a = ds
-#     a = normalize(a, ds_norm_largest)
-#
-#     return X, s0, t, a
-#
-#
-# # invert normalization
-# def denormalize(value, largest):
-#     return value * float(largest)
-#
-#
-# # apply normalization
-# def normalize(value, largest):
-#     return value / largest
-#
-# # define data parameters
-# seed(1)
-# n_examples = 10000
-# n_dim = 20
-# # define training options
-# n_batch = 200
-# n_epoch = 50
-#
-#
-# ################################################################
-# # Model Dynamics
-# ################################################################
-#
-# # create model dynamics
-#
-# # specify input placeholders
-# md_input = tf.placeholder(tf.float32, [None, n_dim * 2])
-#
-# # specify network structure
-# md_initializer = tf.glorot_uniform_initializer()
-# md_dense1 = tf.layers.dense(inputs=md_input, units=4 * n_dim, kernel_initializer=md_initializer)
-# md_dense2 = tf.layers.dense(inputs=md_dense1, units=2 * n_dim, kernel_initializer=md_initializer)
-#
-# # specify output
-# md_output = tf.layers.dense(inputs=md_dense2, units=1 * n_dim, kernel_initializer=md_initializer)
-#
-# # network target for training
-# md_target = tf.placeholder(tf.float32, [None, n_dim * 1])
-#
-# # loss
-# md_loss = tf.losses.mean_squared_error(md_target, md_output)
-#
-# # train step
-# md_global_step = tf.train.create_global_step()
-# md_train_step = tf.train.AdadeltaOptimizer(learning_rate=1.0).minimize(loss=md_loss, global_step=md_global_step)
-#
-# ################################################################
-# # Policy
-# ################################################################
-#
-# # create policy
-# with tf.variable_scope('policy'):
-#     # specify input placeholders
-#     policy_input = tf.placeholder(tf.float32, [None, n_dim * 2])
-#
-#     # specify network structure
-#     policy_initializer = tf.glorot_uniform_initializer()
-#     policy_dense1 = tf.layers.dense(inputs=policy_input, units=4 * n_dim, kernel_initializer=policy_initializer)
-#     policy_dense2 = tf.layers.dense(inputs=policy_dense1, units=2 * n_dim, kernel_initializer=policy_initializer)
-#
-#     # specify output
-#     policy_output = tf.layers.dense(inputs=policy_dense2, units=1 * n_dim, kernel_initializer=policy_initializer)
-#
-#     # network target for training
-#     policy_target = tf.placeholder(tf.float32, [None, n_dim * 1])
-#
-#     # trainable variables
-#     trainable_vars = tf.trainable_variables('policy')
-#
-#     # train step
-#     policy_global_step = tf.Variable(0., trainable=False)
-#
-#     ############################################
-#     # Policy gradient based optimization routine
-#     ############################################
-#
-#     policy_md_loss = md_loss
-#     action_grads = tf.gradients(ys=policy_md_loss, xs=md_input)[0][:, n_dim:]
-#     policy_opt = tf.train.AdadeltaOptimizer(learning_rate=1.0)
-#     optimize_op = policy_opt.minimize(loss=policy_output, global_step=policy_global_step, var_list=trainable_vars,
-#                                       grad_loss=action_grads)
-#
-#
-#
-# # Open the session
-# sess = tf.InteractiveSession()
-# sess.run(tf.global_variables_initializer())
-#
-#
-# ########################################################
-# # Train Model Dynamics
-# ########################################################
-#
-# # train loop
-# for _ in range(n_epoch):
-#     # generate new dataset for each epoch
-#     X, Y = generate_model_dynamics_training_data(n_examples)
-#
-#     for i in range(n_examples // n_batch):
-#         # get next batch
-#         x_batch = X[i * n_batch : (i + 1) * n_batch]
-#         y_batch = Y[i * n_batch: (i + 1) * n_batch]
-#
-#         # train step
-#         feed_dict = {md_input: x_batch, md_target: y_batch}
-#         md_train_step.run(feed_dict=feed_dict)
-#
-#     # calculate loss on the whole dataset
-#     y_pred = sess.run(md_output, feed_dict={md_input:X})
-#     total_loss = np.mean(np.sum(np.square(Y - y_pred), axis=1), axis=0)
-#     y_pred_denormed = denormalize(y_pred, largest)
-#     y_denormed = denormalize(Y, largest)
-#     total_loss_denormed = np.mean(np.sum(np.square(y_denormed - y_pred_denormed), axis=1), axis=0)
-#     print('|epoch : {}| model dynamics loss: {:.10f}| unnormalized loss: {:.7f}'.format(_, total_loss, total_loss_denormed))
-#
-# # Final evaluation on some new patterns
-# X, Y = generate_model_dynamics_training_data(n_examples)
-# y_pred = sess.run(md_output, feed_dict={md_input:X})
-# # calculate error
-# expected = denormalize(Y, largest)
-# predicted = denormalize(y_pred, largest)
-# rmse = sqrt(mean_squared_error(expected, predicted))
-# print('RMSE: %f' % rmse)
-# # show some examples
-# for i in range(5):
-#     error = expected[i] - predicted[i]
-#     print('Expected={}, Predicted={} (err={})'.format(expected[i], predicted[i], error))
-#
-#
-# ########################################################
-# # Train Policy
-# ########################################################
-#
-# # train loop
-# for _ in range(n_epoch):
-#     # generate new dataset for each epoch
-#     X, s0, t, Y = generate_policy_training_data(n_examples)
-#
-#     for i in range(n_examples // n_batch):
-#         # get next batch
-#         x_batch = X[i * n_batch: (i + 1) * n_batch]
-#         y_batch = Y[i * n_batch: (i + 1) * n_batch]
-#         s0_batch = s0[i * n_batch: (i + 1) * n_batch]
-#         t_batch = t[i * n_batch: (i + 1) * n_batch]
-#
-#         # train step
-#         y_pred = sess.run(policy_output, feed_dict={policy_input: x_batch})
-#         md_x_batch = np.concatenate((s0_batch, y_pred), axis=1)
-#
-#         feed_dict = {policy_input: x_batch, md_input: md_x_batch, md_target: t_batch}
-#         optimize_op.run(feed_dict=feed_dict)
-#     # calculate loss on the whole dataset
-#     y_pred = sess.run(policy_output, feed_dict={policy_input: X})
-#     total_loss = np.mean(np.sum(np.square(Y - y_pred), axis=1), axis=0)
-#     y_pred_denormed = denormalize(y_pred, ds_norm_largest)
-#     y_denormed = denormalize(Y, ds_norm_largest)
-#     total_loss_denormed = np.mean(np.sum(np.square(y_denormed - y_pred_denormed), axis=1), axis=0)
-#     print('|epoch : {}|policy loss: {:.10f}| unnormalized loss: {:.7f}'.format(_, total_loss, total_loss_denormed))
-#
-# # Final evaluation on some new patterns
-# X, s0, t, Y = generate_policy_training_data(n_examples)
-# y_pred = sess.run(policy_output, feed_dict={policy_input:X})
-# # calculate error
-# expected = denormalize(Y, ds_norm_largest)
-# predicted = denormalize(y_pred, ds_norm_largest)
-# rmse = sqrt(mean_squared_error(expected, predicted))
-# print('RMSE: %f' % rmse)
-# # show some examples
-# for i in range(5):
-#     error = expected[i] - predicted[i]
-#     print('Expected={}, Predicted={} (err={})'.format(expected[i], predicted[i], error))
