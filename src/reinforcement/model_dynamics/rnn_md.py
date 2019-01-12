615e559e4a58475005710390c389c508378ad3dd
qdenisq <qdenisq@gmail.com>
2019-01-12 17:00:48 +1100

    DS: update rnn model dynamics (seems to almost work now goes to loss < 2.0 for 64-dimensional state space)

--- src/reinforcement/model_dynamics/rnn_md.py
+++ src/reinforcement/model_dynamics/rnn_md.py
@@ -3,14 +3,15 @@ import numpy as np
 import pandas as pd
 import torch
 
-from torch import nn
+from torch.nn import Linear, LSTM, Tanh, ReLU, Module, MSELoss
+from torchvision import transforms
 from pprint import pprint
 
 from src.speech_classification.audio_processing import AudioPreprocessor
 from src.speech_classification.pytorch_conv_lstm import LstmNet
 
 
-class LstmModelDynamics(nn.Module):
+class LstmModelDynamics(Module):
     def __init__(self, **kwargs):
         super(LstmModelDynamics, self).__init__()
         self.__acoustic_state_dim = kwargs['acoustic_state_dim']
@@ -18,29 +19,41 @@ class LstmModelDynamics(nn.Module):
         self.__action_dim = kwargs['state_dim']
         self.__hidden_cells = kwargs['hidden_reccurent_cells_count']
 
-        self.__lstm = nn.LSTM(self.__acoustic_state_dim + self.__state_dim + self.__action_dim ,
+        self.__bn1 = torch.nn.BatchNorm1d(self.__acoustic_state_dim + self.__state_dim + self.__action_dim)
+
+        self.__lstm = LSTM(self.__acoustic_state_dim + self.__state_dim + self.__action_dim,
                               self.__hidden_cells, batch_first=True)
-        self.__output_layer = nn.Linear(self.__hidden_cells, self.__acoustic_state_dim)
+        self.__linear_layer = Linear(self.__hidden_cells, 256)
+        self.__output_layer = Linear(256, self.__acoustic_state_dim)
+
 
     def forward(self, acoustic_states, states, actions, hidden=None):
+
         x = torch.cat((acoustic_states, states, actions), -1)
+        original_dim = x.shape
+        x = self.__bn1(x.view(-1, original_dim[-1]))
+        x = x.view(original_dim)
         lstm_output, hidden = self.__lstm(x, hidden)
-        x = self.__output_layer(lstm_output)
+        x = lstm_output
+        x = Tanh()(self.__linear_layer(x))
+        x = self.__output_layer(x)
         return lstm_output, x
 
 
 def train(*args, **kwargs):
     print(kwargs)
 
+    device = kwargs['train']['device']
+
     # 1. Init audio preprocessing
     preproc = AudioPreprocessor(**kwargs['preprocessing_params'])
     sr = kwargs['preprocessing_params']['sample_rate']
 
     # 2. Load preprocessing net
-    preproc_net = torch.load(kwargs['preproc_net_fname'])
+    preproc_net = torch.load(kwargs['preproc_net_fname']).to(device)
 
     # 3. Init model dynamics net
-    md_net = LstmModelDynamics(**kwargs['model_dynamics_params'])
+    md_net = LstmModelDynamics(**kwargs['model_dynamics_params']).to(device)
     optim = torch.optim.Adam(md_net.parameters(), lr=kwargs['train']['learning_rate'], eps=kwargs['train']['learning_rate_eps'])
 
     # 4. Load training set
@@ -49,6 +62,7 @@ def train(*args, **kwargs):
 
     # 5. Train loop
     params = kwargs['train']
+    md_net.train()
     for i in range(params['num_steps']):
         sample = df.sample(n=kwargs['train']['minibatch_size'])
         states = np.stack(sample.loc[:, 'states'].values)
@@ -56,27 +70,36 @@ def train(*args, **kwargs):
         audio = np.stack(sample.loc[:, 'audio'].values)
 
         preproc_audio = np.array([preproc(audio[j], sr) for j in range(audio.shape[0])])
-        _, _, acoustic_states = preproc_net(torch.from_numpy(preproc_audio).float(),
+
+        acoustic_states = torch.from_numpy(preproc_audio).float().to(device)
+        # acoustic_states = acoustic_states.view(-1, kwargs['model_dynamics_params']["acoustic_state_dim"])
+        # mean_norm = acoustic_states.mean(dim=0)
+        # mean_std = acoustic_states.std(dim=0)
+        # acoustic_states = (acoustic_states - mean_norm.view(1, -1)) / mean_std.view(1, -1)
+        # acoustic_states = acoustic_states.view(kwargs['train']['minibatch_size'], -1, kwargs['model_dynamics_params']["acoustic_state_dim"])
+        _, _, acoustic_states = preproc_net(torch.from_numpy(preproc_audio).float().to(device),
                                  seq_lens=np.array([preproc_audio.shape[-2]]))
 
 
 
         seq_len = actions.shape[1]
+        acoustic_state_dim = kwargs['model_dynamics_params']["acoustic_state_dim"]
 
         # forward prop
         lstm_outs, predicted_acoustic_states = md_net(acoustic_states,
-                                           torch.from_numpy(states[:, :seq_len, :]).float(),
-                                           torch.from_numpy(actions).float())
+                                           torch.from_numpy(states[:, :seq_len, :]).float().to(device),
+                                           torch.from_numpy(actions).float().to(device))
 
         # compute error
-        loss = nn.MSELoss()(acoustic_states[:, 1:, :], predicted_acoustic_states[:, :-1, :])
+        loss = MSELoss(reduction='sum')(predicted_acoustic_states[:, :-1, :].contiguous().view(-1, acoustic_state_dim),
+                                        acoustic_states[:, 1:, :].contiguous().view(-1, acoustic_state_dim)) / (seq_len * kwargs['train']['minibatch_size'])
 
         # backprop
         optim.zero_grad()
         loss.backward()
         optim.step()
 
-        print("\rstep :{} | loss: {:.4f}".format(i, loss.detach().item()), end="")
+        print("\rstep: {} | loss: {:.4f}".format(i, loss.detach().cpu().item()), end="")
 
 
 if __name__ == '__main__':
