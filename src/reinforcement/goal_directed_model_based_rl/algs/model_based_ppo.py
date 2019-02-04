DS: add model based ppo (in progress: need to fix masking for proper reward calculation)

src/reinforcement/goal_directed_model_based_rl/algs/model_based_ppo.py
@@ -1,36 +1,30 @@
+import json
+from pprint import pprint
+import os
 import numpy as np
+import pandas as pd
+import datetime
+
 import torch
+from src.reinforcement.goal_directed_model_based_rl.algs.ppo import PPO
 
 
-class PPO:
-    def __init__(self, agent=None, **kwargs):
-        self.agent = agent
+class ModelBasedPPO(PPO):
+    def __init__(self, agent=None, model_dynamics=None, **kwargs):
+        super(ModelBasedPPO, self).__init__(agent=agent, **kwargs)
+        self.model_dynamics = model_dynamics
+        self.model_dynamics_optim = torch.optim.Adam(self.model_dynamics.parameters(), lr=kwargs['model_dynamics_lr'], eps=kwargs['learning_rate_eps'])
 
-        self.actor_optim = torch.optim.Adam(agent.get_actor_parameters(), lr=kwargs['actor_lr'], eps=kwargs['learning_rate_eps'])
-        self.critic_optim = torch.optim.Adam(agent.get_critic_parameters(), lr=kwargs['critic_lr'], eps=kwargs['learning_rate_eps'])
-
-        self.num_epochs_actor = kwargs['num_epochs_actor']
-        self.num_epochs_critic = kwargs['num_epochs_critic']
-        self.discount = kwargs['discount']
-        self.lmbda = kwargs['lambda']
-        self.minibatch_size = kwargs['minibatch_size']
-        self.epsilon = kwargs['epsilon']
-        self.beta = kwargs['beta']
-        self.clip_grad = kwargs['clip_grad']
-        self.device = kwargs['device']
-        self.num_rollouts_per_update = kwargs['rollouts_per_update']
-
-    def rollout(self, env, num_rollouts):
+    def virtual_rollout(self, env, num_rollouts):
         """
-           Runs an agent in the environment and collects trajectory
-           :param env: Environment to run the agent in (ReacherEnvironment)
-           :return states: (torch.Tensor)
-           :return actions: (torch.Tensor)
-           :return rewards: (torch.Tensor)
-           :return dones: (torch.Tensor)
-           :return values: (torch.Tensor)
-           :return old_log_probs: (torch.Tensor)
-           """
+          Runs an agent in the "virtual environment" using trained model dynamics and collects trajectory
+          :return states: (torch.Tensor)
+          :return actions: (torch.Tensor)
+          :return rewards: (torch.Tensor)
+          :return dones: (torch.Tensor)
+          :return values: (torch.Tensor)
+          :return old_log_probs: (torch.Tensor)
+          """
         states_out = []
         actions_out = []
         rewards_out = []
@@ -39,6 +33,11 @@ class PPO:
         old_log_probs_out = []
         for i in range(num_rollouts):
             state = env.reset()
+            state = env.normalize(state, env.state_bound)
+            reference = env.get_current_reference()
+
+            cur_step = 0
+            episode_length = len(reference)
             # Experiences
             states = []
             actions = []
@@ -48,15 +47,22 @@ class PPO:
             old_log_probs = []
 
             self.agent.eval()
+            self.model_dynamics.eval()
             # Rollout
             while True:
-                state = env.normalize(state, env.state_bound)
                 action, old_log_prob, _, value = self.agent(torch.from_numpy(state).float().to(self.device).view(1, -1))
                 action = np.clip(action.detach().cpu().numpy(), -1., 1.)
-                _, old_log_prob, _, _ = self.agent(torch.from_numpy(state).float().to(self.device).view(1, -1), torch.from_numpy(action).float().to(self.device))
+                _, old_log_prob, _, _ = self.agent(torch.from_numpy(state).float().to(self.device).view(1, -1),
+                                                   torch.from_numpy(action).float().to(self.device))
 
                 action_denorm = env.denormalize(action.squeeze(), env.action_bound)
-                next_state, reward, done, _ = env.step(action_denorm)
+                next_state = self.model_dynamics(torch.from_numpy(state).float().to(self.device).view(1, -1),
+                                                 torch.from_numpy(action).float().to(self.device))
+
+                goal = reference[cur_step]
+                reward = env._reward(next_state, goal, action)
+                cur_step += 1
+                done = cur_step >= episode_length
 
                 states.append(state)
                 actions.append(action.squeeze())
@@ -66,7 +72,6 @@ class PPO:
                 old_log_probs.append(old_log_prob.detach().cpu().numpy())
 
                 state = next_state
-                env.render()
                 if np.any(done):
                     break
 
@@ -87,6 +92,8 @@ class PPO:
 
         return states_out, actions_out, rewards_out, dones_out, values_out, old_log_probs_out
 
+
+
     def train(self, env, num_episodes):
         """
         Train the agent to solve environment
@@ -97,9 +104,13 @@ class PPO:
 
         scores = []
         for episode in range(num_episodes):
-            states, actions, rewards, dones, values, old_log_probs = self.rollout(env, self.num_rollouts_per_update)
+            # states, actions, rewards, dones, values, old_log_probs = self.rollout(env, self.num_rollouts_per_update)
+            states, actions, rewards, dones, values, old_log_probs = self.virtual_rollout(env, self.num_rollouts_per_update)
 
-            score = rewards.sum(dim=-1).mean()
+
+
+
+            score = rewards.sum(dim=0).mean()
 
             T = rewards.shape[0]
             last_advantage = torch.zeros((rewards.shape[1], 1))
@@ -115,8 +126,8 @@ class PPO:
 
             # Update
             returns = returns.view(-1, 1)
-            states = states.view(-1, env.state_dim)
-            actions = actions.view(-1, env.action_dim)
+            states = states.view(-1, env.get_state_dim())
+            actions = actions.view(-1, env.get_action_dim())
             old_log_probs = old_log_probs.view(-1, 1)
 
             # update critic
@@ -180,3 +191,14 @@ class PPO:
 
         print("Training finished. Result score: ", score)
         return scores
+
+
+
+if __name__ == '__main__':
+    with open('rnn_md_config.json') as data_file:
+        data = json.load(data_file)
+
+    pprint(data)
+    kwargs = data
+
+    train(**kwargs)

src/reinforcement/goal_directed_model_based_rl/env.py
@@ -144,7 +144,7 @@ class VTLEnvWithReferenceTransition(VTLEnvPreprocAudio):
 
         # add reference both tract_glottis and goal_audio in the state space
         self.goal_dim = self.state_dim
-        self.goal_bound = self.state_bound
+        self.goal_bound = self.state_bound.copy()
         self.state_dim += self.goal_dim
         self.state_bound.extend(self.goal_bound)
         self.current_reference_idx = 0
@@ -168,7 +168,7 @@ class VTLEnvWithReferenceTransition(VTLEnvPreprocAudio):
         info = None
         return state_out, reward, done, info
 
-    def _reward(self, state, goal):
+    def _reward(self, state, goal, action):
         res = -np.sum((state[-self.state_dim:] - goal) ** 2)
         return res
 
@@ -181,6 +181,8 @@ class VTLEnvWithReferenceTransitionMasked(VTLEnvWithReferenceTransition):
         self.original_state_bound = self.state_bound
         self.original_action_dim = self.action_dim
         self.original_action_bound = self.action_bound
+        self.original_goal_dim = self.goal_dim
+        self.original_goal_bound = self.goal_bound
 
         vtl_names = self.tract_param_names + self.glottis_param_names
         self.state_mask = np.zeros(self.original_state_dim, dtype=bool)
@@ -195,7 +197,6 @@ class VTLEnvWithReferenceTransitionMasked(VTLEnvWithReferenceTransition):
                     self.state_mask[idx] = 1
                 except ValueError:
                     raise ValueError("unrecognised parameter name in VTL: {}".format(name))
-
 
         self.goal_mask_names = kwargs['goal_parameters_selected']
         self.goal_mask = np.zeros(self.goal_dim, dtype=bool)
@@ -222,10 +223,12 @@ class VTLEnvWithReferenceTransitionMasked(VTLEnvWithReferenceTransition):
                 raise ValueError("unrecognised parameter name in VTL: {}".format(name))
 
         self.state_dim = int(sum(self.state_mask))
+        self.goal_dim = int(sum(self.goal_mask))
         self.action_dim = int(sum(self.action_mask))
 
         self.state_bound = np.array(self.state_bound)[np.array(self.state_mask)]
         self.action_bound = np.array(self.action_bound)[np.array(self.action_mask)]
+        self.goal_bound = np.array(self.goal_bound)[np.array(self.goal_mask)]
 
     def reset(self, state_to_reset=None, **kwargs):
         state_out = super(VTLEnvWithReferenceTransition, self).reset(state_to_reset)
@@ -244,15 +247,21 @@ class VTLEnvWithReferenceTransitionMasked(VTLEnvWithReferenceTransition):
         done = False
         if self.current_step >= self.references[self.current_reference_idx].shape[0] - 1:
             done = True
-        reward = self._reward(np.array(state_out)[self.goal_mask], np.array(goal)[self.goal_mask])
+        s_m_normed = self.normalize(np.array(state_out)[self.goal_mask], np.array(self.original_state_bound[:self.original_goal_dim])[self.goal_mask])
+        g_m_normed = self.normalize(np.array(goal)[self.goal_mask], np.array(self.original_state_bound[:self.original_goal_dim])[self.goal_mask])
+        reward = self._reward(s_m_normed, g_m_normed, action)
         state_out = np.concatenate((state_out, goal))
         state_out = state_out[self.state_mask]
         info = None
         return state_out, reward, done, info
 
-    def _reward(self, state, goal):
-        res = -np.sum((state[-self.state_dim:] - goal) ** 2)
+    def _reward(self, state, goal, action):
+        res = -np.sum((state[-len(goal):] - goal) ** 2)
         return res
+
+    def get_current_reference(self):
+        return self.references[self.current_reference_idx][:, self.goal_mask]
+
 
 
 

src/reinforcement/goal_directed_model_based_rl/model.py
@@ -77,11 +77,12 @@ class SimpleStochasticActorCritic(Module):
             x = self.relu(x)
         mu = self.tanh(self.mu(x))
 
-        log_var = -2. - self.relu(self.log_var(x))
+        log_var = - self.relu(self.log_var(x))
         sigmas = log_var.exp().sqrt() + 1.0e-5
         dists = Normal(mu, sigmas)
         if action is None:
             action = dists.sample()
+
         log_prob = dists.log_prob(action).sum(dim=-1, keepdim=True)
 
         x = state
@@ -102,6 +103,59 @@ class SimpleStochasticActorCritic(Module):
 #################################################################################
 # MODEL DYNAMICS
 #################################################################################
+
+class SimpleStochasticModelDynamics(Module):
+    def __init__(self, **kwargs):
+        super(SimpleStochasticModelDynamics, self).__init__()
+        self.__acoustic_state_dim = kwargs['goal_dim']
+        self.__state_dim = kwargs['action_dim']
+        self.__action_dim = kwargs['state_dim']
+        self.__linears_size = kwargs['linear_layers_size']
+
+        input_size = self.__acoustic_state_dim + self.__state_dim + self.__action_dim
+        self.__bn1 = torch.nn.BatchNorm1d(input_size)
+
+        self.linears = ModuleList([Linear(input_size, self.__linears_size[0])])
+        self.linears.extend(
+            [Linear(self.__linears_size[i - 1], self.__linears_size[i]) for i in range(1, len(self.__linears_size))])
+
+        self.goal_mu = Linear(self.__linears_size[-1], kwargs['goal_dim'])
+        self.goal_log_var = Linear(self.__linears_size[-1], kwargs['goal_dim'])
+
+        self.state_mu = Linear(self.__linears_size[-1], kwargs['state_dim'])
+        self.state_log_var = Linear(self.__linears_size[-1], kwargs['state_dim'])
+
+        self.relu = ReLU()
+        self.tanh = Tanh()
+
+        self.apply(init_weights)  # xavier uniform init
+
+    def forward(self, states, actions):
+        x = torch.cat((states, actions), -1)
+        original_dim = x.shape
+        x = self.__bn1(x.view(-1, original_dim[-1]))
+        x = x.view(original_dim)
+
+        for linear in self.linears:
+            x = self.relu(linear(x))
+
+        # predict state
+        state_mu = self.tanh(self.state_mu(x))
+        state_log_var = -self.relu(self.state_log_var(x))
+        state_sigmas = state_log_var.exp().sqrt()
+        state_dists = Normal(state_mu, state_sigmas + 1.0e-4)
+        states = state_dists.rsample()
+        state_log_prob = state_dists.log_prob(states).sum(dim=-1, keepdim=True)
+
+        # predict goal
+        goal_mu = self.tanh(self.goal_mu(x))
+        goal_log_var = -self.relu(self.goal_log_var(x))
+        goal_sigmas = goal_log_var.exp().sqrt()
+        goal_dists = Normal(goal_mu, goal_sigmas + 1.0e-4)
+        goals = goal_dists.rsample()
+        goal_log_prob = goal_dists.log_prob(goals).sum(dim=-1, keepdim=True)
+
+        return states, goals, state_log_prob, goal_log_prob, state_dists, goal_dists
 
 
 class StochasticLstmModelDynamics(Module):

src/reinforcement/vtl_reference_guided/pytorch_train.py
@@ -149,7 +149,7 @@ def train(settings, env, replay_buffer, reference_trajectory):
 
     md_net = ModelDynamicsNet(s_dim, g_dim, a_dim)
     md_criterion = nn.MSELoss()
-    md_optimizer = torch.optim.Adadelta(md_net.parameters())
+    md_optimizer = torch.optim.Adam(md_net.parameters())
 
     ################################################################
     # Policy
@@ -184,7 +184,7 @@ def train(settings, env, replay_buffer, reference_trajectory):
             return num_features
 
     policy_net = PolicyNet(s_dim, g_dim, a_dim)
-    policy_optimizer = torch.optim.Adadelta(policy_net.parameters())
+    policy_optimizer = torch.optim.Adam(policy_net.parameters())
 
     dt = str(datetime.datetime.now().strftime("%m_%d_%Y_%I_%M_%p"))
     # writer = tf.summary.FileWriter(settings['summary_dir'] + '/summary_md_' + dt, sess.graph)
@@ -199,7 +199,7 @@ def train(settings, env, replay_buffer, reference_trajectory):
     # main train loop
     ###########################################
     train_step_i = 0
-
+    r = []
     for i in range(num_episodes):
         # pick random initial state from the reference trajectory
         s0_index = randrange(0, reference_trajectory.shape[0] - 1)
@@ -210,7 +210,8 @@ def train(settings, env, replay_buffer, reference_trajectory):
         g0 = s0
         env.reset(s0)
 
-        r = []
+
+        ret = 0.
         # rollout episode
         for j in range(s0_index, len(reference_trajectory) - 1):
             target = reference_trajectory[j + 1]
@@ -242,8 +243,13 @@ def train(settings, env, replay_buffer, reference_trajectory):
             g1_normed = normalize(g1, g_bound)
             tar_normed = normalize(target, g_bound)
             miss = abs(tar_normed - g1_normed)
+
             if i % save_step != 0:
                 replay_buffer.add(s0, g0, action, s1, g1, target)
+
+            if i % save_step == 0:
+                ret += np.sum((tar_normed - g1_normed)**2)
+
             s0 = s1
             g0 = g1
             # some sort of early stopping
@@ -253,8 +259,10 @@ def train(settings, env, replay_buffer, reference_trajectory):
 
         # dump video
         if i % save_step == 0 and replay_buffer.size() > minibatch_size:
+            r.append(ret)
             fname = video_dir + '/episode_' + str(datetime.datetime.now().strftime("%m_%d_%Y_%I_%M_%p_%S"))
             env.dump_episode(fname)
+            pickle.dump(r, open( "return.pckl", "wb"))
         # train model_dynamics and policy
 
         if replay_buffer.size() > minibatch_size:
@@ -293,6 +301,18 @@ def train(settings, env, replay_buffer, reference_trajectory):
                 md_loss.backward()
                 md_optimizer.step()
 
+            for _ in range(n_train_steps):
+                # train_step_i += 1
+                s0_batch, g0_batch, a_batch, s1_batch, g1_batch, target_batch = \
+                    replay_buffer.sample_batch(minibatch_size)
+
+                # normalize data before train
+                s0_batch_normed = normalize(s0_batch, s_bound)
+                g0_batch_normed = normalize(g0_batch, g_bound)
+                a_batch_normed = normalize(a_batch, a_bound)
+                s1_batch_normed = normalize(s1_batch, s_bound)
+                g1_batch_normed = normalize(g1_batch, g_bound)
+                target_batch_normed = normalize(target_batch, g_bound)
 
                 #############################################################
                 # train policy
@@ -331,10 +351,10 @@ def train(settings, env, replay_buffer, reference_trajectory):
                 loss.backward()
                 policy_optimizer.step()
 
-                expected_actions_normed = target_batch_normed - g0_batch_normed
+                expected_actions_normed = normalize(denormalize(target_batch_normed - g0_batch_normed, g_bound), a_bound)
                 policy_loss_out = np.mean(np.sum(np.square(expected_actions_normed - actions_normed.detach().numpy()), axis=1), axis=0)
 
-                print("|episode: {}| train step: {}| model_dynamics loss: {:.8f}| policy loss: {:.5f}".format(i, train_step_i, md_loss.detach().numpy(), policy_loss_out))
+            print("|episode: {}| train step: {}| model_dynamics loss: {:.8f}| policy loss: {:.5f}".format(i, train_step_i, md_loss.detach().numpy(), policy_loss_out))
 
 
 
@@ -354,8 +374,8 @@ def main():
             'goal_bound': env.state_bound,
             'episode_length': 40,
             'minibatch_size': 512,
-            'max_train_per_simulation': 50,
-            'save_video_step': 200,
+            'max_train_per_simulation': 100,
+            'save_video_step': 20,
 
             'actor_tau': 0.01,
             'actor_learning_rate': 0.001,
@@ -368,7 +388,7 @@ def main():
 
     replay_buffer = ReplayBuffer(100000)
 
-    reference_fname = r'C:\Study\SpeechAcquisitionModel\src\VTL\references\a_o.pkl'
+    reference_fname = r'C:\Study\SpeechAcquisitionModel\src\VTL\references\a_u.pkl'
     with open(reference_fname, 'rb') as f:
         (tract_params, glottis_params) = pickle.load(f)
         target_trajectory = np.hstack((np.array(tract_params), np.array(glottis_params)))