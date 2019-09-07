eb9637bd84a09ff68433a80f10079c13fbfe04c8
qdenisq <qdenisq@gmail.com>
2019-09-07 13:05:08 +1000

    DS: add classificsation ce loss and train net on both bce and ce simaltaniously. also add tensorboard logging

--- src/siamese_net_sound_similarity/train.py
+++ src/siamese_net_sound_similarity/train.py
@@ -101,35 +101,54 @@ class SiameseSpeechCommandsDataCollector(SpeechCommandsDataCollector):
                 'y': duplicate_labels,
                 'seq_len': seq_lens}
 
-class LstmNet(nn.Module):
+
+class SiameseLSTMNet(nn.Module):
     def __init__(self, model_settings):
-        super(LstmNet, self).__init__()
+        super(SiameseLSTMNet, self).__init__()
         self.__n_window_height = model_settings['dct_coefficient_count']
         self.__n_classes = model_settings['label_count']
         self.__n_hidden_cells = model_settings['hidden_reccurent_cells_count']
         self.__dropout = nn.Dropout(p=0.2)
         self.__bn1 = nn.BatchNorm1d(self.__n_window_height)
-        self.__compress_layer = nn.Linear(self.__n_hidden_cells, 10)
+        # self.__compress_layer = nn.Linear(self.__n_hidden_cells, 10)
         self.__lstm = nn.LSTM(self.__n_window_height, self.__n_hidden_cells, batch_first=True, bidirectional=False)
-        self.__output_layer = nn.Linear(self.__n_hidden_cells, 256)
-        self.__output_layer_1 = nn.Linear(256, self.__n_classes)
 
-    def forward(self, x, seq_lens, hidden=None):
+
+        self.__output_layer = nn.Linear(self.__n_hidden_cells * 2, 512)
+        self.__output_layer_1 = nn.Linear(512, 1)
+
+        self.__output_layer_cce = nn.Linear(self.__n_hidden_cells, 512)
+        self.__output_layer_cce_1 = nn.Linear(512, self.__n_classes)
+
+    def forward(self, input, hidden=None):
         # if hidden is None:
         #     hidden = torch.zeros(x.size(0), self.n_hidden)
-        orig_shape = x.shape
-        x = x.view(-1, self.__n_window_height)
-        x = self.__bn1(x)
-        x = self.__dropout(x)
-        x = x.view(orig_shape)
-        x, hidden = self.__lstm(x, hidden)
-        lstm_out = x
-        last_elements = seq_lens - 1
-        # adjust x to the actual seq length for every sequence
-        x = torch.nn.ReLU()(self.__output_layer(lstm_out))
-        pred_full = self.__output_layer_1(x)
-        x = pred_full[np.arange(0, len(seq_lens)), last_elements, :]
-        return x, hidden, lstm_out, pred_full
+        lstm_out = []
+        for i in range(2):
+            x = input[i]
+            orig_shape = x.shape
+            x = x.view(-1, self.__n_window_height)
+            x = self.__bn1(x)
+            # # x = self.__dropout(x)
+            x = x.view(orig_shape)
+            x, hidden = self.__lstm(x, None)
+            lstm_out.append(hidden[0])
+
+        # cce path
+        cce_output = torch.cat(lstm_out, dim=1).squeeze()
+        cce_output = torch.nn.ReLU()(self.__output_layer_cce(cce_output))
+        cce_output = self.__output_layer_cce_1(cce_output)
+
+        # bce path
+        lstm_out = torch.cat(lstm_out, dim=-1).squeeze()
+        output = torch.nn.Tanh()(self.__output_layer(lstm_out))
+        output = torch.nn.Sigmoid()(self.__output_layer_1(output))
+        return output, cce_output
+
+
+def accuracy(out, labels):
+    outputs = np.argmax(out, axis=1)
+    return np.sum(outputs == labels, axis=None) / float(labels.size)
 
 
 def train():
@@ -153,8 +172,7 @@ def train():
         'label_count': len(wanted_words_combined) + 2,
         'hidden_reccurent_cells_count': 128,
         'winlen': 0.04,
-        'winstep': 0.04,
-        'num_nets': 20
+        'winstep': 0.04
     }
 
     save_dir = r'C:\Study\SpeechAcquisitionModel\models\siamese_net_sound_similarity'
@@ -174,31 +192,71 @@ def train():
                                             validation_percentage=10
                                             )
 
-    # configure training procedure
-    n_train_steps = 1000
-    n_mini_batch_size = 256
+    # Summary writer
+    dt = str(datetime.datetime.now().strftime("%m_%d_%Y_%I_%M_%p"))
+    writer = SummaryWriter(f'../../reports/seamise_net_{dt}')
 
+    # configure training procedure
+    n_train_steps = 10000
+    n_mini_batch_size = 64
+
+    siamese_net = SiameseLSTMNet(model_settings).to('cuda')
+    optimizer = torch.optim.Adam(siamese_net.parameters(), lr=0.0005)
+
+    dummy_input = torch.zeros(2, 1, 25, 26).cuda()
+    writer.add_graph(siamese_net, dummy_input)
     """
     Train 
     """
 
     for i in range(n_train_steps):
         # collect data
-        d = data_iter.get_data(n_mini_batch_size, 0, 'training')
-        data = d['x']
-        # cut first 2 steps
-        data = d['x'][:, 2:, :]
-        labels = d['y']
+        data = data_iter.get_data(n_mini_batch_size, 0, 'training')
+        labels = data['y']
 
-        duplicates = data_iter. get_duplicates(labels, 0, 'training')
+        duplicates = data_iter.get_duplicates(labels, 0, 'training')
+        assert np.any(labels == duplicates['y'])
+
         non_duplicates = data_iter.get_nonduplicates(labels, 0, 'training')
+        assert np.any(labels != non_duplicates['y'])
 
-        k = 2
+        # construct a tensor of a form [data duplicates, data non-duplicates]
+        x = torch.from_numpy(np.array([np.concatenate([data['x'], data['x']]),
+                                       np.concatenate([duplicates['x'], non_duplicates['x']])])).float().to('cuda')
+        y_target = np.array([0] * len(labels) + [1]*len(labels))
+        y_target = torch.from_numpy(y_target).float().to('cuda')
+        # forward
+        y, predicted_labels = siamese_net(x)
+
+        #
+        target_labels = torch.from_numpy(np.array([np.concatenate([data['y'],
+                                                                   data['y'],
+                                                                   duplicates['y'],
+                                                                   non_duplicates['y']])]
+                                                  )).long().to('cuda').squeeze()
+
+
+
+        # backward and update
+        optimizer.zero_grad()
+        bce_loss = nn.BCELoss()(y, y_target)
+        ce_loss = torch.nn.CrossEntropyLoss()(predicted_labels, target_labels)
+        # loss = nn.BCELoss()(y, y_target) + torch.nn.CrossEntropyLoss()(predicted_labels, target_labels)
+        loss = bce_loss + ce_loss
+        loss.backward()
+        optimizer.step()
+
+        acc = accuracy(predicted_labels.detach().cpu().numpy(), target_labels.detach().cpu().numpy())
+        writer.add_scalar('BCE', bce_loss.detach().cpu(), i)
+        writer.add_scalar('CCE', ce_loss.detach().cpu(), i)
+        writer.add_scalar('Classification Accuracy', acc, i)
+
+        if i % 100 == 0:
+            for name, param in siamese_net.named_parameters():
+                writer.add_histogram("SiameseNet_" + name, param, i)
+
+        print(i, loss.detach().cpu(), bce_loss.detach().cpu(), ce_loss.detach().cpu())
         # seq_lengths = d['seq_len']
-
-
-
-
 
 
 if __name__ == '__main__':
