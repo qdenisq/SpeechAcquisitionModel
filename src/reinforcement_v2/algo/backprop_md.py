DS: major commit in DTW (add shane settle structure, fix sdtw, refactor) and keep working on backprop in speech acquisition model

src/reinforcement_v2/algo/backprop_md.py
@@ -15,7 +15,7 @@ import torch.optim as optim
 from src.reinforcement_v2.envs.env import EnvironmentManager
 
 from src.reinforcement_v2.common.replay_buffer import ReplayBuffer
-from src.reinforcement_v2.common.nn import SoftQNetwork, PolicyNetwork, ModelDynamics
+from src.reinforcement_v2.common.nn import SoftQNetwork, PolicyNetwork, ModelDynamics, DeterministicPolicyNetwork
 
 from src.reinforcement_v2.common.tensorboard import DoubleSummaryWriter
 from src.reinforcement_v2.common.noise import OUNoise
@@ -46,7 +46,7 @@ class BackpropIntoPolicy(nn.Module):
             self.policy_net = torch.load(kwargs['pretrained_policy']).policy_net
             self.policy_net.to(self.device)
         else:
-            self.policy_net = PolicyNetwork(**kwargs['policy_network']).to(self.device)
+            self.policy_net = DeterministicPolicyNetwork(**kwargs['policy_network']).to(self.device)
 
         self.use_alpha = kwargs.get('use_alpha', True)
         if not self.use_alpha:
@@ -73,7 +73,7 @@ class BackpropIntoPolicy(nn.Module):
         self.replay_buffer = ReplayBuffer(self.replay_buffer_size, self.replay_buffer_csv_filename, init_data_fname)
 
         self.gamma = kwargs.get('gamma', 0.99)
-        self.num_updates_per_step = kwargs.get("num_updates_per_step", 5)
+        self.num_updates_per_step = kwargs.get("num_updates_per_step", 30)
         self.train_step = 0
         self.frame_idx = 0
 
@@ -107,15 +107,16 @@ class BackpropIntoPolicy(nn.Module):
         Policy loss
         """
 
-        new_action, log_prob, epsilon, mean, log_std = self.policy_net.evaluate(state)
+        new_action = self.policy_net(state)
         predicted_next_agent_state = self.model_dynamics(agent_state, new_action)
         predicted_next_agent_state_masked = predicted_next_agent_state[:, reference_mask]
-        state_masked = state[:, reference_mask]
+        state_masked = state[:, self.agent_state_dim:]
         policy_loss = torch.nn.SmoothL1Loss(reduction="sum")(predicted_next_agent_state_masked, state_masked) / batch_size
 
         self.md_optimizer.zero_grad()
         self.policy_optimizer.zero_grad()
         policy_loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.policy_net.parameters(), 1)
         self.policy_optimizer.step()
 
         self.train_step += 1
@@ -200,24 +201,19 @@ class BackpropIntoPolicy(nn.Module):
 
             for step in range(max_steps):
                 timer['algo'].start()
-                action, pi_mean, pi_log_std, log_prob = self.policy_net.get_action(state)
-                log_prob = log_prob.sum(dim=1).detach().cpu().numpy()
-                action = action.detach()
-                # ??? not sure if pi_entropy is calculated correctly
-                pi_entropy = pi_log_std.sum(dim=1, keepdim=True)
-                pi_entropy = pi_entropy.detach().cpu().numpy()
-                pi_mean = pi_mean.detach().cpu().mean().numpy()
-                pi_log_std = pi_log_std.detach().cpu().mean().numpy()
-                # log_prob = Normal(pi_mean, pi_log_std.exp()).log_prob(action.to(self.device)).sum(dim=1).detach().cpu().numpy()
-                action = action.numpy()
+                action = self.policy_net.get_action(state)
+                action = action.detach().cpu().numpy()
                 if not self.use_alpha:
                     action = action + self.noise.sample().reshape(*action.shape) * self.noise_level
-                    # action = np.clip(action, 0, 1)
+                    # just for testing
+                    # action *= 0
+                    action = np.clip(action, -1, 1)
 
                 timer['algo'].stop()
                 timer['env'].start()
 
-                next_state, reward, done, _ = env.step(action)
+                next_state, reward, done, info = env.step(action)
+                # dtw_dist = info['dtw_dist']
                 if kwargs['visualize']:
                     env.render()
 
@@ -244,7 +240,7 @@ class BackpropIntoPolicy(nn.Module):
                 writer.add_histogram("Action", action, self.frame_idx)
                 timer['utils'].stop()
 
-                if len(self.replay_buffer) > 1 * batch_size:
+                if len(self.replay_buffer) > 3 * batch_size and self.frame_idx % 25 == 0:
                     timer['algo'].start()
                     for _ in range(self.num_updates_per_step):
                         _, policy_loss, md_loss = self.update(batch_size)
@@ -255,23 +251,21 @@ class BackpropIntoPolicy(nn.Module):
                         writer.add_scalar('Noise_level', self.noise_level, self.frame_idx)
                     writer.add_scalar('Policy_loss', policy_loss, self.frame_idx)
                     writer.add_scalar('Model Dynamics Loss', md_loss, self.frame_idx)
-                    writer.add_scalar('Policy entropy', pi_entropy.mean(), self.frame_idx)
-                    writer.add_scalar('Policy mean action', pi_mean, self.frame_idx)
-                    writer.add_scalar('Policy log std action', pi_log_std, self.frame_idx)
-                    writer.add_scalar('Action probability', log_prob.mean(), self.frame_idx)
-
+                    print(f"policy_loss: {policy_loss:.2f} | md_loss: {md_loss:.2f}")
                     # explicitly remove all tensor-related variables just to ensure well-behaved memory management
-                    del policy_loss, md_loss, pi_entropy,\
-                        pi_mean, pi_log_std, log_prob, action
+                    del policy_loss, md_loss, action
                     timer['utils'].stop()
 
 
                 # ADD termination condition:
+                current_steps = env.get_attr('current_step')
+                print(current_steps)
                 for w in range(kwargs['num_workers']):
-                    if reward[w] < 0.001 and reward[w] > 0: # dtw > 7
+                    if reward[w] < 0.001 and current_steps[w] > 3: # dtw > 7
                         done[w] = True
 
                 if any(done):
+
                     timer['utils'].start()
                     for i in range(kwargs['num_workers']):
                         if done[i]:
@@ -279,6 +273,7 @@ class BackpropIntoPolicy(nn.Module):
                             rewards.append(ep_reward[i])
                             ep_reward[i] = 0.
                             state[i] = env.reset([i])
+                    env.render()
 
                     writer.add_scalar("Reward", reward_running, self.frame_idx)
                     for k, v in timer.items():
@@ -371,7 +366,7 @@ if __name__ == '__main__':
         os.makedirs(run_dir)
     except:
         pass
-    with open(os.path.join(run_dir, 'SoftActorCritic.yaml'), 'w') as f:
+    with open(os.path.join(run_dir, 'md_backprop.yaml'), 'w') as f:
         yaml.dump(kwargs, f)
 
 

src/reinforcement_v2/envs/dtw_we_env.py
@@ -105,10 +105,10 @@ class VTLDTWEnv(VTLEnvPreprocAudio):
 
         reward = dist
         if not np.isnan(reward):
-            reward = np.exp(5. - reward)
+            reward = np.exp(0. - reward)
         else:
             reward = 0.
-        info = {}
+        info = {'dtw_dist': dist}
         
         self.current_state = state_out
 

src/siamese_net_sound_similarity/train_v2_with_settle_structure.py
@@ -11,6 +11,9 @@ import dtwalign
 import numpy as np
 from six.moves import xrange
 
+import yaml
+
+from pprint import pprint
 from python_speech_features import mfcc
 import scipy.io.wavfile as wav
 
@@ -110,22 +113,37 @@ class SiameseDeepLSTMNet(nn.Module):
         super(SiameseDeepLSTMNet, self).__init__()
         self.__n_window_height = model_settings['mfcc_num']
         self.__n_classes = model_settings['label_count']
-        self.__n_hidden_reccurent_cells = model_settings['hidden_reccurent_cells_count']
-        self.__n_hidden_cells = model_settings['hidden_latent_count']
-        self.__dropout = nn.Dropout(p=0.2)
+
+        self.__dropout = nn.Dropout(p=model_settings['dropout'])
         self.__bn1 = nn.BatchNorm1d(self.__n_window_height)
-        # self.__compress_layer = nn.Linear(self.__n_hidden_cells, 10)
-        self.__lstm_0 = nn.LSTM(self.__n_window_height, self.__n_hidden_reccurent_cells, batch_first=True, bidirectional=False)
-        self.__lstm_1 = nn.LSTM(self.__n_hidden_reccurent_cells, self.__n_hidden_reccurent_cells, batch_first=True,
-                                bidirectional=False)
 
-        self.__linear_0 = nn.Linear(self.__n_hidden_reccurent_cells, self.__n_hidden_cells)
-        self.__linear_1 = nn.Linear(self.__n_hidden_cells, self.__n_hidden_cells)
-        self.__linear_2 = nn.Linear(self.__n_hidden_cells, self.__n_hidden_cells)
+        self.__n_hidden_reccurent = model_settings['hidden_reccurent']
+        self.__n_hidden_fc = model_settings['hidden_fc']
 
-        self.__output_layer = nn.Linear(self.__n_hidden_cells * 2, 1)
+        self.lstms = nn.ModuleList([nn.LSTM(self.__n_window_height, self.__n_hidden_reccurent[0], batch_first=True, bidirectional=False)])
+        self.lstms.extend(
+            [nn.LSTM(self.__n_hidden_reccurent[i-1], self.__n_hidden_reccurent[i], batch_first=True, bidirectional=False)
+             for i in range(1, len(self.__n_hidden_reccurent))])
 
-        self.__output_layer_cce = nn.Linear(self.__n_hidden_cells, self.__n_classes)
+        self.linears = nn.ModuleList(
+            [nn.Linear(self.__n_hidden_reccurent[-1], self.__n_hidden_fc[0])])
+        self.linears.extend(
+            [nn.Linear(self.__n_hidden_fc[i-1], self.__n_hidden_fc[i]) for i in range(1, len(self.__n_hidden_reccurent))])
+
+        # # self.__compress_layer = nn.Linear(self.__n_hidden_cells, 10)
+        # self.__lstm_0 = nn.LSTM(self.__n_window_height, self.__n_hidden_reccurent_cells, batch_first=True, bidirectional=False)
+        # self.__lstm_1 = nn.LSTM(self.__n_hidden_reccurent_cells, self.__n_hidden_reccurent_cells, batch_first=True,
+        #                         bidirectional=False)
+        # self.__lstm_2 = nn.LSTM(self.__n_hidden_reccurent_cells, self.__n_hidden_reccurent_cells, batch_first=True,
+        #                         bidirectional=False)
+        #
+        # self.__linear_0 = nn.Linear(self.__n_hidden_reccurent_cells, self.__n_hidden_cells)
+        # self.__linear_1 = nn.Linear(self.__n_hidden_cells, self.__n_hidden_cells)
+        # self.__linear_2 = nn.Linear(self.__n_hidden_cells, self.__n_hidden_cells)
+
+        self.__output_layer = nn.Linear(self.__n_hidden_fc[-1] * 2, 1)
+
+        self.__output_layer_cce = nn.Linear(self.__n_hidden_fc[-1], self.__n_classes)
 
     def single_forward(self, input, hidden=None):
         x = input
@@ -134,11 +152,27 @@ class SiameseDeepLSTMNet(nn.Module):
         x = self.__bn1(x)
         x = self.__dropout(x)
         x = x.view(orig_shape)
-        x, hidden = self.__lstm_0(x, None)
-        x, hidden = self.__lstm_1(x, None)
-        x = torch.relu(self.__linear_0(x))
-        x = torch.relu(self.__linear_1(x))
-        x = torch.relu(self.__linear_2(x))
+
+        for i in range(len(self.__n_hidden_reccurent) - 1):
+            x, hidden = self.lstms[i](x, None)
+            x = self.__dropout(x)
+        x, hidden = self.lstms[-1](x, None)
+
+        for i in range(len(self.__n_hidden_fc) - 1):
+            x = torch.relu(self.linears[i](x))
+            x = self.__dropout(x)
+        x = torch.relu(self.linears[-1](x))
+
+        # x, hidden = self.__lstm_0(x, None)
+        # x = self.__dropout(x)
+        # x, hidden = self.__lstm_1(x, None)
+        # x = self.__dropout(x)
+        # x, hidden = self.__lstm_2(x, None)
+        # x = torch.relu(self.__linear_0(x))
+        # x = self.__dropout(x)
+        # x = torch.relu(self.__linear_1(x))
+        # x = self.__dropout(x)
+        # x = torch.relu(self.__linear_2(x))
 
         hidden = x[:, -1, :]
         return x, hidden
@@ -176,7 +210,7 @@ def accuracy(out, labels):
     return np.sum(outputs == labels, axis=None) / float(labels.size)
 
 
-def train():
+def train(config):
     #############################################################
     # Simple Lstm train script
     #############################################################
@@ -185,30 +219,18 @@ def train():
     Initialize
     """
 
-    wanted_words = ['bed', 'bird', 'cat', 'dog', 'down', 'eight', 'five', 'four', 'go', 'happy', 'house', 'left',
-                    'marvin',
-                    'nine', 'no', 'off', 'on', 'one', 'right', 'seven', 'sheila', 'six', 'stop', 'three', 'tree', 'two',
-                    'up', 'wow', 'yes', 'zero']
+    wanted_words = config['wanted_words']
 
     wanted_words_combined = wanted_words
 
-    model_settings = {
-        'dct_coefficient_count': 13,
-        'mfcc_num': 39,
-        'label_count': len(wanted_words_combined) + 2,
-        'hidden_reccurent_cells_count': 512,
-        'hidden_latent_count': 32,
-        'winlen': 0.02,
-        'winstep': 0.01,
-        'open_end': False,
-        'dist': 'l1',
-        'margin': 0.4
-    }
+    model_settings = config
+    model_settings['label_count'] = len(wanted_words_combined) + 2
+
     open_end = model_settings['open_end']
     dist = model_settings['dist']
     margin = model_settings['margin']
 
-    save_dir = r'C:\Study\SpeechAcquisitionModel\models\siamese_net_sound_similarity'
+    save_dir = config['save_dir']
     if not os.path.exists(save_dir):
         try:
             os.makedirs(save_dir)
@@ -229,12 +251,15 @@ def train():
     dt = str(datetime.datetime.now().strftime("%m_%d_%Y_%I_%M_%p"))
     writer = SummaryWriter(f'../../reports/seamise_net_{dt}')
 
+    with open(os.path.join(f'../../reports/seamise_net_{dt}/', 'config.yaml'), 'w') as f:
+        yaml.dump(config, f)
+
     # configure training procedure
-    n_train_steps = 100000
-    n_mini_batch_size = 64
+    n_train_steps = config['train_steps']
+    n_mini_batch_size = config['mini_batch_size']
 
     siamese_net = SiameseDeepLSTMNet(model_settings).to('cuda')
-    optimizer = torch.optim.Adam(siamese_net.parameters(), lr=0.0005)
+    optimizer = torch.optim.Adam(siamese_net.parameters(), lr=config['learning_rate'])
 
     soft_dtw_loss = SoftDTW(open_end=open_end, dist=dist)
 
@@ -243,7 +268,7 @@ def train():
     """
     Train 
     """
-    max_bce_acc = 0.
+    max_cce_acc = 0.
     for i in range(n_train_steps):
         # collect data
         data = data_iter.get_data(n_mini_batch_size, 0, 'training')
@@ -279,38 +304,60 @@ def train():
         # ce loss
         ce_loss = torch.nn.CrossEntropyLoss()(predicted_labels, target_labels)
 
-        Triplet_loss_weight = anneal_function('logistic', i, 0.0025, 2500)
-
+        Triplet_loss_weight = anneal_function('logistic', i, config['triplet_anneal_k'], config['triplet_anneal_b'])
         Triplet_loss = torch.tensor([0]).float().cuda()
 
-        # DTWLoss (want to minimize dtw between duplica)
-        DTW_loss = torch.tensor([0]).float().cuda()
-        for k in range(n_mini_batch_size):
-            DTW_loss += torch.nn.functional.relu(soft_dtw_loss(zs[0][k], zs[1][k]) -
-                                                 soft_dtw_loss(zs[0][k + n_mini_batch_size], zs[1][k + n_mini_batch_size])
-                                                 + margin)
+        if config['loss_type'] == 'sdtw':
 
-        if open_end:
-            DTW_loss /= (n_mini_batch_size)
+            # DTWLoss (want to minimize dtw between duplica)
+            DTW_loss = torch.tensor([0]).float().cuda()
+            for k in range(n_mini_batch_size):
+                DTW_loss += torch.nn.functional.relu(soft_dtw_loss(zs[0][k], zs[1][k]) -
+                                                     soft_dtw_loss(zs[0][k + n_mini_batch_size], zs[1][k + n_mini_batch_size])
+                                                     + margin)
+
+            if open_end:
+                DTW_loss /= (n_mini_batch_size)
+            else:
+                # DTW_loss /= (n_mini_batch_size * zs[0].shape[1])
+                DTW_loss /= (n_mini_batch_size)
+            Triplet_loss = DTW_loss
+            loss = 0.2 * ce_loss + 0.8 * Triplet_loss * Triplet_loss_weight
+
+        elif config['loss_type'] == 'l2':
+            L2_loss = torch.tensor([0]).float().cuda()
+            for k in range(n_mini_batch_size):
+                L2_loss += torch.nn.functional.relu(torch.sum((zs[0][k] - zs[1][k])**2, dim=-1)[-1] -
+                                                    torch.sum((zs[0][k + n_mini_batch_size] - zs[1][k + n_mini_batch_size]) ** 2, dim=-1)[-1] +
+                                                    margin)
+
+            if open_end:
+                L2_loss /= (n_mini_batch_size)
+            else:
+                L2_loss /= (n_mini_batch_size * zs[0].shape[1])
+            Triplet_loss = L2_loss
+            loss = 0.2 * ce_loss + 0.8 * Triplet_loss * Triplet_loss_weight
+
+        elif config['loss_type'] == 'cos_hinge':
+            Cos_hinge_loss = torch.tensor([0]).float().cuda()
+            for k in range(n_mini_batch_size):
+                Cos_hinge_loss += torch.nn.functional.relu(
+                    - torch.nn.CosineSimilarity(dim=0)(zs[0][k], zs[1][k]) +
+                    torch.nn.CosineSimilarity(dim=0)(zs[0][k + n_mini_batch_size],
+                                                     zs[1][k + n_mini_batch_size])
+                    + margin)
+
+            Cos_hinge_loss /= n_mini_batch_size
+            Triplet_loss = Cos_hinge_loss
+            loss = 0.2 * ce_loss + 0.8 * Cos_hinge_loss * Triplet_loss_weight
+        elif config['loss_type'] == 'ce':
+            loss = ce_loss
         else:
-            DTW_loss /= (n_mini_batch_size * zs[0].shape[1])
-        Triplet_loss = DTW_loss
-
-        # L2_loss = torch.tensor([0]).float().cuda()
-        # for k in range(n_mini_batch_size):
-        #     L2_loss += torch.nn.functional.relu(torch.sum((zs[0][k] - zs[1][k])**2, dim=-1)[-1] -
-        #                                         torch.sum((zs[0][k + n_mini_batch_size] - zs[1][k + n_mini_batch_size]) ** 2, dim=-1)[-1] +
-        #                                         margin)
-        #
-        # if open_end:
-        #     L2_loss /= (n_mini_batch_size)
-        # else:
-        #     L2_loss /= (n_mini_batch_size * zs[0].shape[1])
-        # Triplet_loss = L2_loss
+            raise KeyError(f"Unknown loss type: {config['loss_type']}")
 
         # loss = bce_loss + ce_loss + KL_loss * KL_weight
         # loss = ce_loss
-        loss = 0.4 * ce_loss + 0.6 * Triplet_loss * Triplet_loss_weight
+        # loss = 0.2 * ce_loss + 0.8 * Triplet_loss * Triplet_loss_weight
         loss.backward()
         optimizer.step()
 
@@ -328,15 +375,14 @@ def train():
             for name, param in siamese_net.named_parameters():
                 writer.add_histogram("SiameseNet_" + name, param, i)
 
-        if bce_acc > max_bce_acc or i % 1000 == 0:
-            if bce_acc > max_bce_acc:
-                max_bce_acc = bce_acc
-            fname = os.path.join(f'../../reports/seamise_net_{dt}/net_{bce_acc}.net')
+        if cce_acc > max_cce_acc or i % 1000 == 0:
+            if cce_acc > max_cce_acc:
+                max_cce_acc = cce_acc
+            fname = os.path.join(f'../../reports/seamise_net_{dt}/net_{i}_{cce_acc}.net')
             torch.save(siamese_net, fname)
 
         print(f"{i} "
               f"| L: {loss.detach().cpu().numpy()} "
-              f"| BCE {bce_loss.detach().cpu().numpy()} "
               f"| CE {ce_loss.detach().cpu().numpy()} "
               f"| Triplet Loss {Triplet_loss.detach().cpu()}")
 
@@ -390,5 +436,12 @@ def train():
 
 
 if __name__ == '__main__':
-    train()
+    with open('configs/ce_dtw_settle_like.yaml', 'r') as data_file:
+        config = yaml.safe_load(data_file)
+    pprint(config)
+
+    train(config)
+
+
+
     print('done')

src/siamese_net_sound_similarity/train_v3_adopted_from_settle.py
@@ -112,11 +112,13 @@ class SiameseDeepLSTMNet(nn.Module):
         self.__n_classes = model_settings['label_count']
         self.__n_hidden_reccurent_cells = model_settings['hidden_reccurent_cells_count']
         self.__n_hidden_cells = model_settings['hidden_latent_count']
-        self.__dropout = nn.Dropout(p=0.2)
+        self.__dropout = nn.Dropout(p=0.3)
         self.__bn1 = nn.BatchNorm1d(self.__n_window_height)
         # self.__compress_layer = nn.Linear(self.__n_hidden_cells, 10)
         self.__lstm_0 = nn.LSTM(self.__n_window_height, self.__n_hidden_reccurent_cells, batch_first=True, bidirectional=False)
         self.__lstm_1 = nn.LSTM(self.__n_hidden_reccurent_cells, self.__n_hidden_reccurent_cells, batch_first=True,
+                                bidirectional=False)
+        self.__lstm_2 = nn.LSTM(self.__n_hidden_reccurent_cells, self.__n_hidden_reccurent_cells, batch_first=True,
                                 bidirectional=False)
 
         self.__linear_0 = nn.Linear(self.__n_hidden_reccurent_cells, self.__n_hidden_cells)
@@ -135,9 +137,14 @@ class SiameseDeepLSTMNet(nn.Module):
         x = self.__dropout(x)
         x = x.view(orig_shape)
         x, hidden = self.__lstm_0(x, None)
+        x = self.__dropout(x)
         x, hidden = self.__lstm_1(x, None)
+        x = self.__dropout(x)
+        x, hidden = self.__lstm_2(x, None)
         x = torch.relu(self.__linear_0(x))
+        x = self.__dropout(x)
         x = torch.relu(self.__linear_1(x))
+        x = self.__dropout(x)
         x = torch.relu(self.__linear_2(x))
 
         hidden = x[:, -1, :]
@@ -152,7 +159,7 @@ class SiameseDeepLSTMNet(nn.Module):
             x = input[i]
             x, hidden = self.single_forward(x)
             lstm_out.append(hidden)
-            zs.append(x)
+            zs.append(hidden)
 
         # cce path
         cce_output = torch.cat(lstm_out, dim=0).squeeze()
@@ -197,7 +204,7 @@ def train():
         'mfcc_num': 39,
         'label_count': len(wanted_words_combined) + 2,
         'hidden_reccurent_cells_count': 512,
-        'hidden_latent_count': 32,
+        'hidden_latent_count': 1024,
         'winlen': 0.02,
         'winstep': 0.01,
         'open_end': False,
@@ -261,7 +268,7 @@ def train():
         y_target = np.array([0] * len(labels) + [1] * len(labels))
         y_target = torch.from_numpy(y_target).float().to('cuda')
         # forward
-        zs, y, predicted_labels = siamese_net(x)
+        embeddings, y, predicted_labels = siamese_net(x)
 
         #
         target_labels = torch.from_numpy(np.array([np.concatenate([data['y'],
@@ -281,20 +288,16 @@ def train():
 
         Triplet_loss_weight = anneal_function('logistic', i, 0.0025, 2500)
 
-        Triplet_loss = torch.tensor([0]).float().cuda()
+        Cos_hinge_loss = torch.tensor([0]).float().cuda()
 
         # DTWLoss (want to minimize dtw between duplica)
-        DTW_loss = torch.tensor([0]).float().cuda()
+        Cos_hinge_loss = torch.tensor([0]).float().cuda()
         for k in range(n_mini_batch_size):
-            DTW_loss += torch.nn.functional.relu(soft_dtw_loss(zs[0][k], zs[1][k]) -
-                                                 soft_dtw_loss(zs[0][k + n_mini_batch_size], zs[1][k + n_mini_batch_size])
+            Cos_hinge_loss += torch.nn.functional.relu( - torch.nn.CosineSimilarity(dim=0)(embeddings[0][k], embeddings[1][k]) +
+                                                 torch.nn.CosineSimilarity(dim=0)(embeddings[0][k + n_mini_batch_size], embeddings[1][k + n_mini_batch_size])
                                                  + margin)
 
-        if open_end:
-            DTW_loss /= (n_mini_batch_size)
-        else:
-            DTW_loss /= (n_mini_batch_size * zs[0].shape[1])
-        Triplet_loss = DTW_loss
+        Cos_hinge_loss /= n_mini_batch_size
 
         # L2_loss = torch.tensor([0]).float().cuda()
         # for k in range(n_mini_batch_size):
@@ -308,9 +311,9 @@ def train():
         #     L2_loss /= (n_mini_batch_size * zs[0].shape[1])
         # Triplet_loss = L2_loss
 
-        # loss = bce_loss + ce_loss + KL_loss * KL_weight
         # loss = ce_loss
-        loss = 0.4 * ce_loss + 0.6 * Triplet_loss * Triplet_loss_weight
+        # loss = bce_loss + ce_loss + KL_loss * KL_weight
+        loss = 0.2 * ce_loss + 0.8 * Cos_hinge_loss * Triplet_loss_weight
         loss.backward()
         optimizer.step()
 
@@ -319,7 +322,7 @@ def train():
         bce_acc = accuracy(y, y_target.detach().cpu().numpy())
         writer.add_scalar('BCE', bce_loss.detach().cpu(), i)
         writer.add_scalar('CCE', ce_loss.detach().cpu(), i)
-        writer.add_scalar('Triplet Loss', Triplet_loss.detach().cpu(), i)
+        writer.add_scalar('Triplet Loss', Cos_hinge_loss.detach().cpu(), i)
         writer.add_scalar('CCE_Accuracy', cce_acc, i)
         writer.add_scalar('BCE_Accuracy', bce_acc, i)
         writer.add_scalar('Triplet_loss_weight', Triplet_loss_weight, i)
@@ -338,7 +341,7 @@ def train():
               f"| L: {loss.detach().cpu().numpy()} "
               f"| BCE {bce_loss.detach().cpu().numpy()} "
               f"| CE {ce_loss.detach().cpu().numpy()} "
-              f"| Triplet Loss {Triplet_loss.detach().cpu()}")
+              f"| Triplet Loss {Cos_hinge_loss.detach().cpu()}")
 
         # TODO: add validation each 1k steps for example
         if i % 500 == 0: